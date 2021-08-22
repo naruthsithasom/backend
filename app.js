436a025c896664f54e2498ef5e571ab996fbb3e8
naruthsithasom <naruth.sithasom@gmail.com>
2021-08-23 02:31:03 +0700

    chagene

--- app.js
+++ app.js
@@ -2,28 +2,25 @@ const express = require('express')
 const app = express()
 const ejs = require('ejs')
 const cors = require('cors')
+const bodyParser = express.urlencoded({extended: false})
+const jobs = [
+  {id:1 , position: "software engineering", level: 'All levels'},
+  {id:2 , position: "project manager",  level: 'Junior'},
+  {id:3 , position: "tester",  level: 'Middle'},
+]
 
 
 app.engine('html',ejs.renderFile)
 app.use(cors())
-app.listen(8000, () => {
-  console.log('Server is up on port: 8000')
-})
+app.listen(8000, () => { console.log('Server is up on port: 8000')})
 
 
-
-const bodyParser = express.urlencoded({extended: false})
 app.get('/insert', insertJobs)
 app.post('/insert', bodyParser ,saveJobs)
-const jobs = [
-  {id:1 , position: "software engineering", res:['sdf','xcv']},
-  {id:2 , position: "software engineering", res:['sdf','xcv']},
-  {id:3 , position: "software engineering", res:['sdf','xcv']},
-]
 
 app.get('/api/jobs', (req, res) => {
   res.header('Access-Control-Allow-Origin','application/json')
-  res.status(200).send(jobs)
+  res.send(jobs)
 })
 
 function insertJobs(req, res){
