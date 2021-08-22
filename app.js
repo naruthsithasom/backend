5d4ecb3a589ff86424b30e109d31c4aaa0a521b2
naruthsithasom <naruth.sithasom@gmail.com>
2021-08-23 02:19:49 +0700

    update

--- app.js
+++ app.js
@@ -1,30 +1,35 @@
 const express = require('express')
 const app = express()
 const ejs = require('ejs')
-const bodyParser = express.urlencoded({extended: false})
+const cors = require('cors')
+
 
 app.engine('html',ejs.renderFile)
+app.use(cors())
+app.listen(8000, () => {
+  console.log('Server is up on port: 8000')
+})
+
+
+
+const bodyParser = express.urlencoded({extended: false})
+app.get('/insert', insertJobs)
+app.post('/insert', bodyParser ,saveJobs)
 const jobs = [
   {id:1 , position: "software engineering", res:['sdf','xcv']},
   {id:2 , position: "software engineering", res:['sdf','xcv']},
   {id:3 , position: "software engineering", res:['sdf','xcv']},
 ]
 
-app.get('/insert', insertJobs)
-app.post('/insert', bodyParser ,saveJobs)
-
 app.get('/api/jobs', (req, res) => {
   res.header('Access-Control-Allow-Origin','application/json')
-   res.send(jobs)
-})
-
-app.listen(8000, () => {
-  console.log('Server is up on port: 8000')
+  res.status(200).send(jobs)
 })
 
 function insertJobs(req, res){
   res.render('index.html')
 }
+
 function saveJobs(req, res){
   let data = [req.body.title, req.body.level, req.body.description, req.body.responsibility]
   if(data){
@@ -32,3 +37,4 @@ function saveJobs(req, res){
   }
   console.log(data)
 }
+
